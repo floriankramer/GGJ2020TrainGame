e21fe7e27538c534a39984c0e6bd781fffe197c7
Stefan <stefan.gerstner96@web.de>
2020-01-31 23:00:51 +0100

    First Commit

---
+++ Train Action 3K/Assets/Scirpts/Train.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Train : MonoBehaviour
+{
+    public static float trainSpeed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

af6be1fdbf4fd08da19be9263a42afa376834eab
Florian Kramer <flo.kramer@arcor.de>
2020-02-01 13:04:50 +0100

    Added a distance counter

---
+++ Train Action 3K/Assets/Scirpts/DistanceCounterScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DistanceCounterScript : MonoBehaviour
+{
+    private Text text;
+    
+    public float distanceTraveled;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distanceTraveled += Train.trainSpeed * Time.deltaTime;
+        text.text = distanceTraveled.ToString("F1") + "m";
+    }
+}

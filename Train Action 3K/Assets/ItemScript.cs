125a7f4fefdfbe9e0dadf8f05b8ff03aaf7f7806
Andreas M. Reumschuessel <andreas@reumschuessel.de>
2020-02-01 01:17:15 +0100

    Updated Item

--- Train Action 3K/Assets/ItemScript.cs
+++ Train Action 3K/Assets/ItemScript.cs
@@ -7,10 +7,12 @@ public class ItemScript : MonoBehaviour
     public RescourceType rescourceType = RescourceType.Wood;
     public float health = 100;
 
+    private BoxCollider2D boxCollider;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        boxCollider = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
@@ -47,7 +49,33 @@ public class ItemScript : MonoBehaviour
 
     void OnMouseUp()
     {
+        Vector3 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        worldPoint.z = 0;
+        Collider2D[] hits = Physics2D.OverlapPointAll(worldPoint);
+        Debug.unityLogger.LogWarning("Item", hits.Length);
+        bool foundStorage = false;
 
+        foreach(Collider2D hit in hits )
+        {
+            if("StorageRoom" == hit.gameObject.tag)
+            {
+                foundStorage = true;
+                transform.SetParent(hit.gameObject.transform);
+                transform.localPosition = new Vector3(0,0,0);
+                break;
+            }
+
+            if("Part" == hit.gameObject.tag)
+            {
+                Consume(health);
+            }
+        }
+
+        if(!foundStorage && transform.parent.parent != null)
+        {
+            // TODO: Where do we parent to?
+            transform.SetParent(transform.parent.parent);
+        }
     }
 
 

fbfc787dd29e773de7517712c90e3e3b6d16e5e2
TomUndSteve <tomUndSteve@unwichtig.com>
2020-02-01 15:01:10 +0100

    Added a very basic barricade (Script + Prefab + Dummy Graphics) git status
    git status

---
+++ Train Action 3K/Assets/Scirpts/Barricade.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Barricade : MonoBehaviour
+{
+    public float damage = 5.0f;
+    public float damagePropagationProbability = 50.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+        if(collision.gameObject.GetComponent<Part>() != null || collision.gameObject.GetComponent<Train>() != null)
+        {
+            Part[] parts = FindObjectsOfType<Part>();
+
+            if(parts.Length > 0)
+            {
+                System.Array.Sort(parts, CompareObjDistance);
+                for(int i = 0; i < parts.Length; i++)
+                {
+                    if(Random.Range(0.0f, 100.0f) < damagePropagationProbability)
+                    {
+                        parts[i].Health -= damage;
+                        Destroy(gameObject);
+                        break;
+                    }
+                }
+            }
+
+            //int randomIndex = Mathf.RoundToInt(Random.Range(0.0f, parts.Length - 1.0f));
+        }
+        
+
+
+    }
+
+    int CompareObjDistance(Part x, Part y)
+    {
+        return y.transform.position.x < x.transform.position.x ? -1 : 1;
+    }
+}

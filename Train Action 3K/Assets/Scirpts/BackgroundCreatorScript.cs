887064dd4b1a3136ffd5855616e63f28c9f1e279
Stefan <stefan.gerstner96@web.de>
2020-02-01 02:46:37 +0100

    Moving Background

--- Train Action 3K/Assets/Scirpts/BackgroundCreatorScript.cs
+++ Train Action 3K/Assets/Scirpts/BackgroundCreatorScript.cs
@@ -4,6 +4,20 @@ using UnityEngine;
 
 public class BackgroundCreatorScript : MonoBehaviour
 {
+    public GameObject[] spawnableObjects;
+
+    private float nextSpawnDistance;
+    private float spawnDistance;
+
+    // Minimal and Maximal Distance that can between two Background Objects 
+    private float minObjectDistance = 0.5f;
+    private float maxObjectDistance = 6f;
+
+    // Values for the Spawning on the Y-Axis
+    private float minObjectYValue = 2;
+    private float maxObjectYValue = 4;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +27,28 @@ public class BackgroundCreatorScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        spawnDistance += Train.trainSpeed * Time.deltaTime;
+        if (nextSpawnDistance < spawnDistance)
+        {
+            SpawnSomething();
+            spawnDistance = 0;
+        }
+    }
+
+    void SpawnSomething()
+    {
+        // TODO add Script that selects a fitting Background Object from the List
+        GameObject spawnThis = spawnableObjects[0];
+
+        // TODO add Script that adds a spawnObjectY infront of the train
+        float spawnObjectY = Random.Range(minObjectYValue, maxObjectYValue);
+
+        // TODO Change 15 to value based on the Screen Size
+        Instantiate(spawnThis, new Vector3(15, spawnObjectY), Quaternion.identity);
+
+        float size = spawnThis.GetComponent<SpriteRenderer>().sprite.rect.size.x/100;
+        float minSpawnDistance = size + minObjectDistance;
+        float maxSpawnDistance = minSpawnDistance + maxObjectDistance;
+        nextSpawnDistance = Random.Range(minSpawnDistance, maxSpawnDistance);
     }
 }
---
+++ Train Action 3K/Assets/Scirpts/BackgroundObject.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundObject : MonoBehaviour
+{
+    public float size;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move with the Speed of the Train
+        float xPosition = transform.position.x + Train.trainSpeed * Time.deltaTime * -1;
+        transform.position = new Vector3(xPosition, transform.position.y);
+
+        // Destroy this Object once it leaves the Screen
+        // TODO Change -15 to a variable that is reacting to the Screen Size
+        if (transform.position.x < -15)
+        {
+            Destroy(gameObject);
+        }
+    }
+}

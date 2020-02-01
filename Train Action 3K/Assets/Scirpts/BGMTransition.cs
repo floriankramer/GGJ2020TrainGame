6e284d27146a8348b213d4ad0ddf8650502511b9
Andreas M. Reumschuessel <andreas@reumschuessel.de>
2020-02-01 21:50:14 +0100

    Added BGM

---
+++ Train Action 3K/Assets/Scirpts/BGMTransition.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGMTransition : MonoBehaviour
+{
+    private static BGMTransition instance;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(instance);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}

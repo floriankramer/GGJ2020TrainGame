f07cbef49c285cf628593fcc413a6b2c5972a0e8
TomUndSteve <tomUndSteve@unwichtig.com>
2020-02-02 14:34:49 +0100

    Applied different damage graphics. Updated the SampleScene with the new super lok! :)

--- Train Action 3K/Assets/Scirpts/WheelScript.cs
+++ Train Action 3K/Assets/Scirpts/WheelScript.cs
@@ -5,16 +5,26 @@ using UnityEngine;
 public class WheelScript : MonoBehaviour
 {
     public float constantSpeed = 0.0f;
+    private bool isAlive = true;
 
     void Start()
     {
-        
+        Part part = gameObject.GetComponent<Part>();
+        part.onDeathEvent.AddListener(onDeath);
+    }
+
+    void onDeath()
+    {
+        isAlive = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float distTravelled = (Train.trainSpeed + constantSpeed) * Time.deltaTime;
-        transform.Rotate(0, 0, -distTravelled / Mathf.PI * 180);
+        if (isAlive)
+        {
+            float distTravelled = (Train.trainSpeed + constantSpeed) * Time.deltaTime;
+            transform.Rotate(0, 0, -distTravelled / Mathf.PI * 180);
+        }
     }
 }

3a11b6d996685e2d257a8d8314aa86606ea5e444
Florian Kramer <flo.kramer@arcor.de>
2020-02-02 11:54:31 +0100

    Added a flamethrower pickup.

---
+++ Train Action 3K/Assets/Scirpts/FlamethrowerPickup.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlamethrowerPickup : MonoBehaviour
+{
+    public float powerTime = 15;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D collision) {
+        FlameThrower[] fts = collision.gameObject.GetComponentsInChildren<FlameThrower>();
+        foreach (FlameThrower ft in fts) {
+            ft.poweredFor += powerTime;
+        }
+        Destroy(gameObject);
+    }
+}
